7763077e198c7dbe54c3c9fb5cea0830675a3691
Jasur <developer7102@gmail.com>
2021-06-08 17:41:49 +0200

    add finish

--- js/main.js
+++ js/main.js
@@ -16,18 +16,48 @@ elsFeaturesTabsLink.forEach(function (link) {
 });
 
 
-var elsQuestionsItem = document.querySelectorAll('.questions__item');
-var elsQuestionsButton = document.querySelectorAll('.questions__item--button');
+var elsFeaturesTabsItem = document.querySelectorAll('.features-tabs__item');
+var elsFeaturesTabsLink = document.querySelectorAll('.features-tabs__link');
 
-
-elsQuestionsButton.forEach(function (button) {
-  button.addEventListener('click', function () {
-
-    elsQuestionsItem.forEach(function (icon) {
-      icon.classList.remove('questions__item--active');
+// Har bir linkka quloq klikka quloq solishni buyuramiz
+elsFeaturesTabsLink.forEach(function (link) {
+  link.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    // Avval hamma li lardan faollik klassini olib tashlaymiz
+    elsFeaturesTabsItem.forEach(function (item) {
+      item.classList.remove('features-tabs__item--active');
     });
 
+    // Bosilgan link ota elementiga faollik klassini qo'shamiz
+    link.parentElement.classList.add('features-tabs__item--active');
+  });
+});
 
-    button.closest('.questions__item').classList.add('questions__item--active');
+// PANELS
+  var elsTabsItem = document.querySelectorAll('.features-tabs__item');
+  var elsTabsLink = document.querySelectorAll('.features-tabs__link');
+  var elsPanel = document.querySelectorAll('.panels__item');
+// Har bir link clickga quloq solsin. Har biriga link deb murojaat qilamiz. O'zimiz nom beramiz
+elsTabsLink.forEach(function (link) {
+  // Har bir linkka click reaksiyasini beramiz
+  link.addEventListener('click', function (evt) {
+    // Linkning tabiiy reaksiyasi (sakrash)ning oldi olinadi
+    evt.preventDefault();
+
+    // Hamma li lardan active klassni olib tashlaymiz. Har biriga item deb murojaat qilamiz
+    elsTabsItem.forEach(function (item) {
+      item.classList.remove('features-tabs__item--active');
+    });
+
+    // Bosilgan linkning otasiga active klassini qo'shamiz
+    link.parentElement.classList.add('features-tabs__item--active');
+
+    // Hamma panellarni yopamiz
+    elsPanel.forEach(function (panel) {
+      panel.classList.remove('panels__item--active');
+    });
+
+    // Bog'langan panelni topamiz
+    document.querySelector(link.getAttribute('href')).classList.add('panels__item--active');
   });
 });
